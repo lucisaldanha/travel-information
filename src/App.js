87813c9e51ffe14b061934c12e27afc56b865cd5
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-07-18 17:46:59 -0400

    first version, need to fix alert prompt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={ 
+      firstName: '', 
+      lastName: '', 
+      isFriendly: false, 
+      textArea: 'Once upon a time...', 
+      gender: '',
+      favColor: 'blue'
+    };
+    this.handleChange= this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    const {name, value, type, checked}= event.target;
+    type === 'checkbox' ? this.setState({[name]: checked}) : this.setState({ [name]: value });
+  };
+
+
+  render() {
+    return (
+      <form >
+        <input 
+          type='text' 
+          value= {this.state.firstName}
+          name='firstName'
+          placeholder='First Name' 
+          onChange={this.handleChange} 
+        />
+        <br />
+        <input 
+          type='text' 
+          value= {this.state.lastName}
+          name='lastName'
+          placeholder='Last Name' 
+          onChange={this.handleChange} 
+        />
+        <br />
+        <textarea 
+          name= 'textArea'
+          value={this.state.textArea} 
+          onChange={this.handleChange}
+          />
+        <br />
+        <label> 
+          <input 
+            type='checkbox'  
+            name='isFriendly'
+            checked={this.state.isFriendly} 
+            onChange={this.handleChange}
+          /> Is Friendly?
+        </label> 
+        <br />
+        <label> 
+          <input 
+            type='radio'  
+            name='gender'
+            value='male'
+            checked={this.state.gender === 'male'} 
+            onChange={this.handleChange}
+          /> Male
+        </label> 
+        <br />
+        <label> 
+          <input 
+            type='radio'  
+            name='gender'
+            value='female'
+            checked={this.state.gender === 'female'} 
+            onChange={this.handleChange}
+          /> Female
+        </label> 
+        <br />
+        <label> Your favorite color is:
+          <select 
+            name='favColor'
+            value={this.state.favColor}
+            onChange={this.handleChange}
+          >
+            <option value='red'>Red</option>
+            <option value='green'>Green</option>
+            <option value='blue'>Blue</option>
+            <option value='pink'>Pink</option>
+          </select>
+        </label> 
+        <p>{this.state.firstName} {this.state.lastName}</p>
+        <p>You are a {this.state.gender}.</p>
+        <p>So your favorite color is: {this.state.favColor}.</p>
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
 
 export default App;
---
+++ src/Apppractice.js
@@ -0,0 +1,194 @@
+import React, {Component} from 'react';
+import './style.css';
+import './img/image4.jpg';
+
+class Apppractice extends Component {
+	constructor(props){
+		super(props);	
+		this.state = {
+			firstName: '',
+			lastName: '',
+			age: '',
+			gender: '',
+			destination: 'Rio de Janeiro - Brazil',
+			vegetarian: false,
+			kosher: false,
+			lactoseFree: false,
+			vegan: false
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		const { name, value, type, checked } = event.target;
+		type === 'checkbox' ? this.setState({[name]: checked}) : this.setState({ [name]: value });
+	}
+
+	handleSubmit(event) {
+		alert(
+		`Passenger info
+		Name: ${this.state.firstName} ${this.state.lastName}
+		Age: ${this.state.age}
+		Gender: ${this.state.gender}
+		Destination: ${this.state.destination}
+		Dietary Restrictions:\n ${(this.state.vegetarian)? 'Vegetarian' : ''}\n ${this.state.kosher ? 'Kosher' : null}\n ${this.state.lactoseFree ? 'Lactose Free' : null}\n ${this.state.vegan ? 'Vegan' : null}
+		` );
+	}
+
+	render() {
+		let arr = [ 
+			this.state.vegetarian, 
+			this.state.kosher, 
+			this.state.lactoseFree,
+			this.state.vegan
+		];
+		let [veg, kos, lact, ven] = arr;
+		let veggie;
+		let kosh;
+		let lacto;
+		let vega;
+		if(veg) {
+			veggie = 'Vegetarian';
+		}
+		if(kos) {
+			kosh = 'Kosher';
+		}
+		if(lact) {
+			lacto = 'Lactose Free';
+		}
+		if(ven) {
+			vega = 'Vegan';
+		}
+
+		let edad = this.state.age;
+		let textAge = edad >= 1 ? (edad + ' years old') : null;
+
+		return (
+			<main>
+				<div className='info'>
+					<h1>Travel form information</h1>
+					<h2>Please complete the form below so we can better serve you.</h2>
+					<hr />
+				</div>
+				<div className='form' >
+					<form onSubmit={this.handleSubmit} >
+						<label><p className='passenger'>Passenger information</p>
+							<label>First Name:
+								<input 
+									value={this.state.firstName}
+									type='text' 
+									placeholder='First Name' 
+									name='firstName' 
+									onChange={this.handleChange}
+								/>
+								<br />
+							</label>
+							<label>Last Name:
+								<input 
+									value={this.state.lastName} 
+									type='text' 
+									placeholder='Last Name' 
+									name='lastName' 
+									onChange={this.handleChange}
+								/>
+								<br />
+							</label>
+							<label>Age:
+								<input 
+									value={this.state.age}  
+									type='number' 
+									min='1'
+									placeholder='Age' 
+									name='age' 
+									onChange={this.handleChange}
+								/>
+								<br /><br />
+							</label>
+							<label>Please select your gender:<br />
+								<div className='flexbox'>
+									<div className='smallerbox'>
+										<input 
+											type='radio' 
+											value='female' 
+											name='gender' 
+											checked={this.state.gender === 'female'} 
+											onChange={this.handleChange} 
+										/>Female<br />
+									</div>
+									<div className='smallerbox'>
+										<input 
+											type='radio' 
+											value='male' 
+											name='gender' 
+											checked={this.state.gender === 'male'} 
+											onChange={this.handleChange} 
+										/>Male<br />
+									</div>	
+									<div className='smallerbox'>
+										<input 
+											type='radio' 
+											value='unknown' 
+											name='gender' 
+											checked={this.state.gender === 'unknown'} 
+											onChange={this.handleChange} 
+										/>Preferred not to answer<br /><br />
+									</div>
+								</div>
+							</label>
+							<label>Please select destination: <br />
+								<select value={this.state.destination} name='destination' onChange={this.handleChange}>
+									<option value='Montreal - Canada'>Montreal - Canada</option>
+									<option value='Rio de Janeiro - Brazil'>Rio de Janeiro - Brazil</option>
+									<option value='Tokyo - Japan'>Tokyo - Japan</option>
+									<option value='Houston TX - USA'>Houston TX - USA</option>
+									<option value='San Francisco CA - USA'>San Francisco CA - USA</option>
+								</select> <br />
+							</label>
+							<br />
+							<label>Dietary restriction(s):<br />
+								<input type='checkbox' 
+									name='vegetarian' 
+									checked={this.state.vegetarian} 
+									onChange={this.handleChange} 
+								/>Vegetarian
+								<input 
+									type='checkbox' 
+									name='kosher' 
+									checked={this.state.kosher} 
+									onChange={this.handleChange} 
+								/>Kosher
+								<input 
+									type='checkbox' 
+									name='lactoseFree' 
+									checked={this.state.lactoseFree} 
+									onChange={this.handleChange} 
+								/>Lactose Free
+								<input 
+									type='checkbox' 
+									name='vegan' 
+									checked={this.state.vegan} 
+									onChange={this.handleChange} 
+								/>Vegan
+							</label>
+						</label> 
+						<br /><br />
+						<button type='submit'>Submit</button>
+					</form>
+				</div>
+				<hr />
+				<div className='completed'>
+					<h4>The information you are going to submit:</h4>
+					<p>Your name: <span style={{color:'white',backgroundColor: 'rgba(171,176,181, 0.4)', fontWeight: 700, textTransform:'uppercase', borderRadius:'2px'}}>{this.state.firstName} {this.state.lastName}</span></p>
+					<p>Age: <span style={{color:'white',backgroundColor: 'rgba(171,176,181, 0.4)', fontWeight: 700, textTransform:'uppercase', borderRadius:'2px'}}>{textAge}</span></p>
+					<p>Gender: <span style={{color:'white',backgroundColor: 'rgba(171,176,181, 0.4)', fontWeight: 700, textTransform:'uppercase', borderRadius:'2px'}}>{this.state.gender}</span></p>
+					<p>Destination: <span style={{color:'white',backgroundColor: 'rgba(171,176,181, 0.4)', fontWeight: 700, textTransform:'uppercase', borderRadius:'2px'}}>{this.state.destination}</span></p>
+					<p>Dietary restriction(s): <span style={{color:'white',backgroundColor: 'rgba(171,176,181, 0.4)', fontWeight: 700, textTransform:'uppercase', borderRadius:'2px'}}>{veggie} {kosh} {lacto} {vega}</span> </p>
+				</div>
+			</main>
+		)
+	}
+}
+
+
+export default Apppractice;
